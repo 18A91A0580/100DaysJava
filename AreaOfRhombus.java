5b4e4193ad32e7a5c0a76520d6271f9b03c92821
Deva Rushi <59044940+18A91A0580@users.noreply.github.com>
2020-08-31 17:29:23 +0530

    Add files via upload

---
+++ AreaOfRhombus.java
@@ -0,0 +1,14 @@
+package basicPrograms;
+import java.util.*;
+public class AreaOfRhombus {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int d1=sc.nextInt();
+		int d2=sc.nextInt();
+		double a=(d1*d2)/2;
+		System.out.println(a);
+
+	}
+
+}
