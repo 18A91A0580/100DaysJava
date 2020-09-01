8d8b83406bf725df48dab731e052004806a6ed35
Deva Rushi <59044940+18A91A0580@users.noreply.github.com>
2020-09-01 17:33:00 +0530

    Add files via upload

---
+++ CollatzSequence.java
@@ -0,0 +1,20 @@
+package prgs;
+
+import java.util.Scanner;
+
+public class CollatzSequence {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number :");
+		int n=sc.nextInt();
+		System.out.print(n+" ");
+		while(n>1) {
+			if(n%2==0) n=n/2;
+			else n=(3*n)+1;
+			System.out.print(n+" ");
+		}
+		
+
+	}
+}
