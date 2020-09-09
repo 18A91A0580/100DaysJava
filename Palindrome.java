7db1977cf8c3e57803a803b3c58e33f22888dc20
Deva Rushi <59044940+18A91A0580@users.noreply.github.com>
2020-09-09 17:33:14 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,29 +1,20 @@
-package basicPrograms;
+package whileLoops;
 import java.util.*;
 public class Palindrome {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter size :");
+		System.out.println("Enter number :");
 		int n=sc.nextInt();
-		int[] a=new int[n];
-		int sum,r,temp;
-		System.out.println("Enter "+n+" values :");
-		for(int i=0;i<n;i++) {
-			a[i]=sc.nextInt();
+		int p=0;
+		int temp=n;
+		while(temp>0) {
+			int l=temp%10;
+			p=p*10+l;
+			temp/=10;
 		}
-		for(int i=0;i<n;i++) {
-			sum=0;
-			temp=a[i];
-			while(temp>0)
-			{
-				r=temp%10;
-				sum=(sum*10)+r;
-				temp/=10;
-			}
-			if(sum==a[i])System.out.println("Palindrome");
-			else System.out.println("Not Palindrome");
-		}
+		if(p==n) System.out.println(n+" is a palindrome");
+		else System.out.println(n+" is not a palindrome");
 	}
 
 }
