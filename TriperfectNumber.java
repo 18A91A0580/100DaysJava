a5747823e670645a0a011b365a0496de4cb43ec6
Deva Rushi <59044940+18A91A0580@users.noreply.github.com>
2020-09-16 17:18:40 +0530

    Add files via upload

---
+++ TriperfectNumber.java
@@ -0,0 +1,21 @@
+package prgs;
+
+import java.util.Scanner;
+
+public class TriperfectNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number :");
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++) {
+			if(n%i==0) {
+				sum+=i;
+			}
+		}
+		if(n*3==sum) System.out.println(n+" is Triperfect number");
+		else System.out.println(n+" is not Triperfect number");
+	}
+
+}
