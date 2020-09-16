abb8a0fe3816da8e7566c8bda15e881660437797
Deva Rushi <59044940+18A91A0580@users.noreply.github.com>
2020-09-16 17:34:05 +0530

    Add files via upload

--- TriperfectNumber.java
+++ TriperfectNumber.java
@@ -9,9 +9,10 @@ public class TriperfectNumber {
 		System.out.println("Enter number :");
 		int n=sc.nextInt();
 		int sum=0;
-		for(int i=1;i<=n;i++) {
+		for(int i=1;i*i<=n;i++) {
 			if(n%i==0) {
-				sum+=i;
+				if(n/i==i) sum+=i;
+				else sum+=i+n/i;
 			}
 		}
 		if(n*3==sum) System.out.println(n+" is Triperfect number");
